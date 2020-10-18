2590c154adb9b1bdaa5b5044e40facee74f7cd52
caiolmf <caio.lmf@gmail.com>
2020-10-18 17:57:54 -0300

    feat: add poloniex service with api calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { subscribe, getCoins, getCoinsPairs } from './services/poloniex';
+
+const App = () => {
+  useEffect(() => {
+    getCoinsPairs()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +27,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
